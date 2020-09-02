b85b8bf8fb7806193fd653665e342ff6e5b11dcb
Dusk Banks <me@bb010g.com>
2020-09-02 16:48:05 -0700

    [meta] update-firefox-nightly: consolidate
    no more manual updates for 2 minutes each time!

--- update-firefox-nightly.sh
+++ update-firefox-nightly.sh
@@ -3,70 +3,24 @@
 
 set -o errexit -o errtrace -o nounset -o pipefail
 shopt -s inherit_errexit
-# Allow ${COMMENT-""} comments (useful during line continuations)
-typeset +gx COMMENT=
+shopt -s extglob
 
-typeset +gf read_nul jq_nul
-read_nul() { IFS= read -r -d '' "$@"; }
-jq_nul() {
-  typeset +gx e; e="$1"; shift
-  jq -j '"\u0000" as $nul | foreach ('"$e"') as $v
-    (2; if . > 0 then . - 1 else . end; (select(. == 0) | $nul), $v)' "$@"
-}
-
-# Stack variable system
-typeset +gx
-
-# # Variable declarations
-# Local scalars
-typeset +gx \
-  ${COMMENT-'"reliable" global variables'} \
-  src_key= \
-  srcs= \
-  ${COMMENT-'reusable working variables'} \
-  entry_key= \
-  entry_value= \
-  result= \
-  ${COMMENT-'array subscript states'} \
-  \
-  \
-  buildhub_filename \
-  buildhub_url \
-  system \
-  timestamp \
-  version \
-  versions_url \
+# variables
+typeset \
+  buildhub_filename='' \
+  buildhub_url='' \
+  date='' \
+  date_d='' \
+  date_m='' \
+  date_y='' \
+  locale='' \
+  system='' \
+  timestamp='' \
+  timestamp_time='' \
+  url='' \
+  version='' \
+  versions_url='' \
   #
-# Local arrays
-typeset +gx -a \
-  #
-# Local associative arrays
-typeset +gx -A \
-  entries \
-  entry_deps \
-  entry_templs \
-  #
-
-src_key=firefox-nightly
-srcs=nix/sources.json
-
-jq_srcs() {
-  jq_nul "$@" --arg src_key "$src_key" "$srcs"
-}
-
-while read_nul entry_key; read_nul entry_value; do
-  entries["$entry_key"]="$entry_value"
-  if [[ entry_key = *_template ]]; then
-    entry_templs["${entry_key#_template}"]="${entry_key}"
-    entry_deps["${entry_key#template}"]=
-  fi
-done < <(jq_srcs '.[$src_key] | to_entries[] | (.key, .value)')
-
-for entry_key in "${!entries[@]}"; do
-  if [[ ! entry_key = *_template ]]; then continue; fi
-
-done
-exit 0
 
 # fetch major version
 versions_url='https://product-details.mozilla.org/1.0/firefox_versions.json'
@@ -83,9 +37,29 @@ buildhub_url="https://download.cdn.mozilla.net/pub/firefox/"\
 timestamp=$(curl -s "${buildhub_url}" | \
   jq -r '.build.date | [scan("\\d+")] | join("-")')
 
+locale=$(jq -r '.["firefox-nightly"].locale' nix/sources.json)
+
+date=${timestamp%%-+([0-9])-+([0-9])-+([0-9])}
+date_y=${date%%-+([0-9])-+([0-9])}
+date_m=${date%%-+([0-9])}
+date_m=${date_m##+([0-9])-}
+date_d=${date##+([0-9])-+([0-9])-}
+slug=firefox-${version}.${locale}.${system}
+timestamp_time=${timestamp##+([0-9])-+([0-9])-+([0-9])-}
+url="https://archive.mozilla.org/pub/firefox/nightly/${date_y}/${date_m}/${timestamp}-mozilla-central/${slug}.tar.bz2"
+
 # merge into sources
 jq -S --indent 4 '.["firefox-nightly"] *= $ARGS.named' \
-  --arg version "$version" --arg timestamp "$timestamp" \
+  --arg buildhub_url "$buildhub_url" \
+  --arg date "$date" \
+  --arg date_d "$date_d" \
+  --arg date_m "$date_m" \
+  --arg date_y "$date_y" \
+  --arg slug "$slug" \
+  --arg timestamp "$timestamp" \
+  --arg timestamp_time "$timestamp_time" \
+  --arg url "$url" \
+  --arg version "$version" \
   nix/sources.json | sponge nix/sources.json
 
 # vim:et:ft=sh:sw=2:tw=78
