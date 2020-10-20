0c63149adbe96d66feec06ffc2f47a5bea5263ee
Dusk Banks <me@bb010g.com>
2020-10-19 22:40:16 -0700

    [modules] secrets: add secrets.geolocation
    ooh, spooky~

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -7,6 +7,18 @@ in
 {
   options = {
     secrets = {
+      # might drop this for geoclude2, b/c this is just consumed by redshift rn
+      geolocation = {
+        latitude = mkOption {
+          type = types.str;
+          description = "Your current latitude, between -90.0 and 90.0";
+        };
+        longitude = mkOption {
+          type = types.str;
+          description = "Your current longitude, between -180.0 and 180.0";
+        };
+      };
+
       tokens = mkOption {
         type = types.attrsOf types.str;
         default = { };
