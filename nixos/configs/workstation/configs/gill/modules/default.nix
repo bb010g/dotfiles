5c52256b44231cac9af87ed0c7b809da554221ed
Dusk Banks <me@bb010g.com>
2024-05-27 19:24:19 -0700

    nixos(gill): enable lingering for user bb010g

--- nixos/configs/workstation/configs/gill/modules/default.nix
+++ nixos/configs/workstation/configs/gill/modules/default.nix
@@ -26,5 +26,7 @@
     #
     # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
     system.stateVersion = "23.11"; # Did you read the comment?
+
+    users.users.bb010g.linger = true;
   };
 }
