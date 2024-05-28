bd44ef8cb076b7dba82b08e2c89bb3fc9fc65687
Dusk Banks <me@bb010g.com>
2024-05-27 18:25:51 -0700

    home-manager: init Element Desktop module
    i'd like it to use Wayland please

---
+++ home-manager/modules/programs/element-desktop.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.programs.element-desktop;
+in
+{
+  options = {
+    programs.element-desktop = {
+      enable = lib.mkEnableOption "the Element Desktop Matrix client";
+      package = lib.mkPackageOption pkgs "Element Desktop" { default = "element-desktop"; };
+      desktopEntry.enableWayland = lib.mkEnableOption "using Wayland when Element Desktop is executed from its desktop entry";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    home.packages = lib.mkMerge [
+      [ cfg.package ]
+      (lib.mkIf cfg.desktopEntry.enableWayland [
+        (lib.hiPrio (pkgs.substitute {
+          pname = "${cfg.package.pname}-wayland";
+          version = cfg.package.version;
+          src = cfg.package + "/share/applications/element-desktop.desktop";
+          dir = "share/applications";
+          substitutions = [
+            "--replace-fail"
+            "Exec=element-desktop"
+            "Exec=env NIXOS_OZONE_WL=1 element-desktop"
+          ];
+        }))
+      ])
+    ];
+  };
+}
