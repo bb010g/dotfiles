77f5b771ba670f505d21b2842564380c37fda3a3
Dusk Banks <me@bb010g.com>
2024-06-04 18:34:11 -0700

    home-manager: actually configure Firefox robustly

--- home-manager/configs/bb010g/modules/firefox.nix
+++ home-manager/configs/bb010g/modules/firefox.nix
@@ -8,7 +8,7 @@ in
 {
   config = lib.mkMerge [
     (lib.mkIf (nixosCfg != null) {
-      programs.firefox.package = nixosCfg.package;
+      programs.firefox.package = nixosCfg.finalPackage;
     })
     {
     }
