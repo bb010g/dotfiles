742ba745ead37fb3b4c725ada797672fd08287a1
Dusk Banks <me@bb010g.com>
2024-07-05 14:18:53 -0700

    flake.lock: Update
    Flake lock file updates:
    
    • Added input 'flake-utils':
        'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a' (2024-03-11)
    • Added input 'flake-utils/systems':
        follows 'systems'
    • Added input 'flakey-profile':
        'github:lf-/flakey-profile/243c903fd8eadc0f63d205665a92d4df91d42d9d' (2024-04-12)
    • Added input 'gitignore':
        'github:hercules-ci/gitignore.nix/637db329424fd7e46cf4185293b9cc8c88c95394' (2024-02-28)
    • Added input 'gitignore/nixpkgs':
        follows 'nixpkgs'
    • Added input 'lix':
        'git+https://git.lix.systems/lix-project/lix.git?ref=refs/heads/main&rev=d00edfb28d0a52d9acd392c582a43f98e773cf4c' (2024-07-04)
    • Added input 'lix-module':
        'git+https://git.lix.systems/lix-project/nixos-module.git?ref=refs/heads/main&rev=5d9d94089fb1ca96222a34bfe245ef5c5ebefd37' (2024-06-25)
    • Added input 'lix-module/flake-utils':
        follows 'flake-utils'
    • Added input 'lix-module/flakey-profile':
        follows 'flakey-profile'
    • Added input 'lix-module/lix':
        follows 'lix'
    • Added input 'lix-module/nixpkgs':
        follows 'nixpkgs'

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-utils.inputs.systems.follows = "systems";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flakey-profile.url = "github:lf-/flakey-profile";
+  inputs.gitignore.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.gitignore.url = "github:hercules-ci/gitignore.nix";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
   inputs.home-manager.url = "github:nix-community/home-manager";
   # inputs.impermanence.url = "github:nix-community/impermanence";
@@ -16,6 +21,13 @@
   inputs.impermanence-contrib.inputs.impermanence.follows = "impermanence";
   inputs.impermanence-contrib.inputs.nixpkgs.follows = "nixpkgs";
   inputs.impermanence-contrib.url = "github:rehno-lindeque/nixos-impermanence";
+  inputs.lix-module.inputs.flake-utils.follows = "flake-utils";
+  inputs.lix-module.inputs.flakey-profile.follows = "flakey-profile";
+  inputs.lix-module.inputs.lix.follows = "lix";
+  inputs.lix-module.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.lix-module.url = "git+https://git.lix.systems/lix-project/nixos-module.git";
+  inputs.lix.flake = false;
+  inputs.lix.url = "git+https://git.lix.systems/lix-project/lix.git";
   inputs.neovim.flake = false;
   inputs.neovim.url = "github:neovim/neovim";
   inputs.nix-flatpak.url = "github:gmodena/nix-flatpak";
