e44391ac12ae4e86408da6f4f0718a3b40369cf9
Dusk Banks <me@bb010g.com>
2025-02-16 03:15:09 -0800

    jujutsu: redesign multiple trunks experiment

--- nix/by-name/jujutsu-bb010g/homeManagerModule.nix
+++ nix/by-name/jujutsu-bb010g/homeManagerModule.nix
@@ -1,19 +1,33 @@
 { homeManagerModules, ... }:
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   inherit (builtins) isNull;
-  inherit (lib.attrsets) mapAttrs;
-  inherit (lib.attrsets) setAttrByPath;
+  inherit (lib.attrsets) concatMapAttrs mapAttrs setAttrByPath;
   inherit (lib.modules) mkDefault mkIf mkMerge mkOverride setDefaultModuleLocation;
+  inherit (lib.strings) removePrefix;
   inherit (lib.trivial) importTOML;
   mapImportModule = importModule: file: setDefaultModuleLocation file (importModule file);
   mapImportModuleTo = f: optionPath: file: mapImportModule (file: { config = setAttrByPath optionPath (f file); }) file;
+  addBuiltinAliases =
+    let
+      removePrefix' = prefix: str:
+        let
+          unprefixedStr = removePrefix prefix str;
+        in
+        if unprefixedStr == str then null else unprefixedStr;
+    in
+    settings: if settings ? revset-aliases then settings // {
+      revset-aliases = concatMapAttrs (builtinName: value: {
+        ${builtinName} = value;
+        ${removePrefix' "builtin_" builtinName} = builtinName;
+      }) settings.revset-aliases or { };
+    } else settings;
 in
 {
   imports = [
     homeManagerModules.jujutsu
-    (mapImportModuleTo (file: mapAttrs (name: mapAttrs (name: mkOverride 1100)) (importTOML file)) [ "programs" "jujutsu" "settings" ] ./_jj-config-builtin.toml)
-    (mapImportModuleTo (file: mapAttrs (name: mapAttrs (name: mkOverride 1050)) (importTOML file)) [ "programs" "jujutsu" "settings" ] ./_jj-config-builtin-trunks.toml)
+    (mapImportModuleTo (file: mapAttrs (name: mapAttrs (name: mkOverride 1100)) (addBuiltinAliases (importTOML file))) [ "programs" "jujutsu" "settings" ] ./_jj-config-builtin.toml)
+    (mapImportModuleTo (file: mapAttrs (name: mapAttrs (name: mkOverride 1050)) (addBuiltinAliases (importTOML file))) [ "programs" "jujutsu" "settings" ] ./_jj-config-builtin-trunks.toml)
     (mapImportModuleTo (file: importTOML file) [ "programs" "jujutsu" "settings" ] ./_jj-config.toml)
   ];
 
@@ -25,6 +39,8 @@ in
       programs.jujutsu.settings.merge-tools.vimdiff.program = "nvim";
       programs.jujutsu.settings.user.email = mkIf (!(isNull config.programs.git.userEmail)) (mkDefault config.programs.git.userEmail);
       programs.jujutsu.settings.user.name = mkIf (!(isNull config.programs.git.userName)) (mkDefault config.programs.git.userName);
+      programs.jujutsu.settings.revset-aliases."immutable_midstream_trunks()" = ''coalesce(builtin_immutable_midstream_trunks(), remote_immutable_trunks(exact:"bb010g"))'';
+      programs.jujutsu.settings.revset-aliases."mutable_midstream_trunks()" = ''coalesce(builtin_mutable_midstream_trunks(), remote_mutable_trunks(exact:"bb010g"))'';
       programs.jujutsu.watchman.enable = true;
     }
     (mkIf config.programs.git.difftastic.enable {
